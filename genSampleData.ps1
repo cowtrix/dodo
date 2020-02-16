ac611bcc0fe742825e39c067de9aab31e2288243
cowtrix <seandgfinnegan@gmail.com>
2020-02-16 22:55:52 +0000

    Sample data gen script: Fix for if manifest file is missing.

--- genSampleData.ps1
+++ genSampleData.ps1
@@ -1,2 +1,3 @@
-rm manifest.log
+rm manifest.log -Force
 dotnet run --project .\test\GenerateSampleData\GenerateSampleData.csproj > manifest.log
+echo "Generated sample data. See manifest.log for GUIDs."
