e244b212a0e190681b45ee0fbd2fb0c0ae5b1e5c
Stuart Quin <stuart.quin@gmail.com>
2020-03-20 14:58:34 +0000

    RE #34 Fetch data for events

--- dodo-frontend/src/app/dodo/routes/rebellion/index.jsx
+++ dodo-frontend/src/app/dodo/routes/rebellion/index.jsx
@@ -1,16 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { fetchRebellion } from "app/domain/services/rebellion";
+import {
+	fetchRebellion,
+	fetchRebellionEvents
+} from "app/domain/services/rebellion";
 import PropTypes from "prop-types";
 
 import RebellionDetail from "app/components/rebellion-detail";
+import RebellionEvents from "app/components/rebellion-events";
+
+import styles from "./rebellion.module.scss";
 
 const Rebellion = ({ match }) => {
 	const { rebellionId } = match.params;
 	const [rebellion, setRebellion] = useState();
+	const [events, setEvents] = useState();
 
 	useEffect(() => {
 		const load = async () => {
 			setRebellion(await fetchRebellion(rebellionId));
+			setEvents(await fetchRebellionEvents(rebellionId));
 		};
 		load();
 	}, [rebellionId]);
@@ -19,17 +27,20 @@ const Rebellion = ({ match }) => {
 		return <div>Loading</div>;
 	}
 
-	console.log(rebellion);
 	return (
-		<div>
-			<RebellionDetail rebellion={rebellion} />
+		<div className={styles.rebellion}>
+			<div className={styles.detail}>
+				<RebellionDetail rebellion={rebellion} />
+			</div>
+			<div className={styles.events}>
+				<RebellionEvents events={events} />
+			</div>
 		</div>
 	);
 };
 
 Rebellion.propTypes = {
-	getRebellions: PropTypes.func,
-	rebellions: PropTypes.array
+	match: PropTypes.object.isRequired
 };
 
 export default Rebellion;
--- dodo-frontend/src/app/domain/services/rebellion.js
+++ dodo-frontend/src/app/domain/services/rebellion.js
@@ -1,4 +1,18 @@
 import { api } from "app/domain/services/api-service";
-import { REBELLIONS } from "app/domain/urls";
+import { SITES, REBELLIONS } from "app/domain/urls";
 
 export const fetchRebellion = rebellionId => api(`${REBELLIONS}${rebellionId}`);
+
+const EVENT_SITE_TYPES = ["Dodo.Sites.EventSite", "Dodo.Sites.MarchSite"];
+
+export const fetchRebellionEvents = async rebellionId => {
+	// TODO this would be better converted in to an API call:
+	// /sites?rebellion={REBELLION_ID}&type={EVENT}
+
+	const sites = await api(SITES);
+	return sites.filter(
+		site =>
+			site.Parent.guid === rebellionId &&
+			EVENT_SITE_TYPES.includes(site.Type)
+	);
+};
