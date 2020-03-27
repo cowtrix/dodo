a844134cccb623fc5bcaa172ab44c8ae8baf166b
jpsingleton <jpsingleton@gmail.com>
2020-03-27 12:02:30 +0000

    remove unnecessary using

--- src/DodoServer/Controllers/Edit/RebellionsController.cs
+++ src/DodoServer/Controllers/Edit/RebellionsController.cs
@@ -4,7 +4,6 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using DodoResources.Rebellions;
 using DodoServer.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
