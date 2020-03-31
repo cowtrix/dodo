eb3f9ddee5229b58b3143faf0e4d4cba3233fbe4
charlestupman <charlie@liontreestudios.co.uk>
2020-03-31 16:23:16 +0100

    added responsive support for header - mobile first

--- dodo-frontend/src/app/dodo/header/login/login.jsx
+++ dodo-frontend/src/app/dodo/header/login/login.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import styles from './login.module.scss'
 
 const loginText = "SIGN IN"
 
-export const Login = ({}) =>
+export const Login = () =>
 	<div className={styles.login}>
 		{loginText}
 	</div>
-
-Login.propTypes = {
-
-}
