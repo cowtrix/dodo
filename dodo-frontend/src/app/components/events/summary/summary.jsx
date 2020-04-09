9685f54d507452a8f5941d0ca268ecb3b90cabb4
charlestupman <charlie@liontreestudios.co.uk>
2020-04-09 12:57:48 +0100

    updated events components

--- dodo-frontend/src/app/components/events/summary/summary.jsx
+++ dodo-frontend/src/app/components/events/summary/summary.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-import { DateTile } from '../../date-tile/index'
-import { Title } from './title/index'
-import { Description } from './description'
-import styles from './summary.module.scss'
-import { Button } from '../../button/index'
+import { DateTile } from "../../date-tile";
+import { Title } from "./title";
+import { Description } from "./description";
+import styles from "./summary.module.scss";
+import { Button } from "../../button";
 
-
-export const Summary = ({ Name, location = 'Glasgow', StartDate, EndDate, PublicDescription, GUID }) =>
+export const Summary = ({
+	Name,
+	location = "Glasgow",
+	StartDate,
+	EndDate,
+	PublicDescription,
+	GUID
+}) => (
 	<li className={styles.eventSummmary}>
-		<Button to={'/rebellion/' + GUID} className={styles.link}>
-			<DateTile startDate={new Date(StartDate)} endDate={new Date(EndDate)}/>
-			<Title title={Name} location={location}/>
-			<Description description={PublicDescription}/>
+		<Button to={"/rebellion/" + GUID} className={styles.link}>
+			<DateTile
+				startDate={new Date(StartDate)}
+				endDate={new Date(EndDate)}
+			/>
+			<Title title={Name} location={location} />
+			<Description description={PublicDescription} />
 		</Button>
 	</li>
+);
 
 Summary.propTypes = {
 	Name: PropTypes.string,
 	location: PropTypes.string,
 	StartDate: PropTypes.string,
 	EndDate: PropTypes.string,
-	summary: PropTypes.string,
-}
+	summary: PropTypes.string
+};
--- dodo-frontend/src/app/components/index.js
+++ dodo-frontend/src/app/components/index.js
@@ -1,14 +1,14 @@
-import * as footer from './footer/title'
-import * as events from './events'
+import * as footer from "./footer/title";
+import * as events from "./events";
 
-export { Map } from './map'
-export { SubHeader } from './sub-header'
-export { Icon } from './icon'
-export { DateTile } from './date-tile'
-export { Button } from './button'
-export { RebellionDetail } from './rebellion-detail'
-export { RebellionEvents } from './rebellion-events'
-export { SiteMap } from './site-map'
+export { Map } from "./map";
+export { SubHeader } from "./sub-header";
+export { Icon } from "./icon";
+export { DateTile } from "./date-tile";
+export { Button } from "./button";
+export { RebellionDetail } from "./rebellion-detail";
+export { RebellionEvents } from "./rebellion-events";
+export { SiteMap } from "./site-map";
+export { Dialog } from "./dialog";
 
-
-export { footer, events }
+export { footer, events };
