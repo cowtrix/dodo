6640a9250cae534650e356719ec0207e5c3bce21
charlestupman <charlie@liontreestudios.co.uk>
2020-04-09 12:52:38 +0100

    updated prop types for date tile

--- dodo-frontend/src/app/components/date-tile/date-tile.jsx
+++ dodo-frontend/src/app/components/date-tile/date-tile.jsx
@@ -1,41 +1,39 @@
-import React from "react"
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-import styles from "./date-tile.module.scss"
-import { getTileColor } from './services'
-
+import styles from "./date-tile.module.scss";
+import { getTileColor } from "./services";
 
 export const DateTile = ({ startDate, endDate = null, backgroundColor }) => {
-	const splitStartDate = `${startDate}`.split(" ")
-	const style = backgroundColor ?
-		{ backgroundColor } :
-		{ backgroundColor: getTileColor(startDate, endDate) }
+	const splitStartDate = `${startDate}`.split(" ");
+	const style = backgroundColor
+		? { backgroundColor }
+		: { backgroundColor: getTileColor(startDate, endDate) };
 
-	const splitEndDate = `${endDate}`.split(" ")
+	const splitEndDate = `${endDate}`.split(" ");
 
-	const dayDate = (endDate) =>
-		!endDate ?
-			splitStartDate[2]:
-			endDate.getMonth() === startDate.getMonth() ?
-					splitStartDate[2] + '-' + splitEndDate[2] :
-					splitStartDate[2] + '-' +splitStartDate[1]
+	const dayDate = endDate =>
+		!endDate
+			? splitStartDate[2]
+			: endDate.getMonth() === startDate.getMonth()
+			? splitStartDate[2] + "-" + splitEndDate[2]
+			: splitStartDate[2] + "-" + splitStartDate[1];
 
-	const monthDate = (endDate) =>
-		!endDate || endDate.getMonth() === startDate.getMonth() ?
-			splitStartDate[1] :
-			'-' + splitEndDate[2] + '-' + splitEndDate[1]
+	const monthDate = endDate =>
+		!endDate || endDate.getMonth() === startDate.getMonth()
+			? splitStartDate[1]
+			: "-" + splitEndDate[2] + "-" + splitEndDate[1];
 
-		return (
-			<div className={styles.dateTile} style={style}>
-				<div className={styles.dayDate}>{dayDate(endDate)}</div>
-				<div className={styles.monthDate}>{monthDate(endDate)}</div>
-			</div>
-		)
-}
+	return (
+		<div className={styles.dateTile} style={style}>
+			<div className={styles.dayDate}>{dayDate(endDate)}</div>
+			<div className={styles.monthDate}>{monthDate(endDate)}</div>
+		</div>
+	);
+};
 
 DateTile.propTypes = {
-	startDate: PropTypes.string,
-	endDate: PropTypes.string,
-	backgroundColor: PropTypes.string,
-}
-
+	startDate: PropTypes.object,
+	endDate: PropTypes.object,
+	backgroundColor: PropTypes.string
+};
