4f5f922575e462b390cf0249098fca03b0985630
cowtrix <seandgfinnegan@gmail.com>
2020-04-18 17:01:06 +0100

    More info in register controller failure message.

--- src/DodoServer/Controllers/Edit/RegisterController.cs
+++ src/DodoServer/Controllers/Edit/RegisterController.cs
@@ -33,7 +33,7 @@ namespace DodoServer.Controllers.Edit
 				var registerResponse = await client.PostAsJsonAsync(REGISTER, model);
 				if (!registerResponse.IsSuccessStatusCode)
 				{
-					ModelState.AddModelError("", $"Register failed: {registerResponse.ReasonPhrase}");
+					ModelState.AddModelError("", $"Register failed: {registerResponse.ReasonPhrase}\n{await registerResponse.Content.ReadAsStringAsync()}");
 					return View(model);
 				}
 
