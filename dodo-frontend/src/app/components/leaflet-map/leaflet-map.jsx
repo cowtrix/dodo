9510f95e931d719da9b419d69f8223ce4e541ca3
charlestupman <charlie@liontreestudios.co.uk>
2020-04-30 17:44:43 +0100

    fix map marker problem

--- dodo-frontend/src/app/components/leaflet-map/leaflet-map.jsx
+++ dodo-frontend/src/app/components/leaflet-map/leaflet-map.jsx
@@ -6,6 +6,8 @@ import { Map } from "react-leaflet"
 import { TitleLayers } from "./title-layers"
 import { Markers } from "./markers"
 
+import styles from "./leaflet-map.module.scss"
+
 const getDefaultCenter = (sites, defaultLocation) => {
 	if (sites && sites.length) {
 		return [sites[0].location.latitude, sites[0].location.longitude]
@@ -15,25 +17,23 @@ const getDefaultCenter = (sites, defaultLocation) => {
 
 export const LeafletMap = ({
 	sites,
-	height = "400px",
 	defaultLocation = [51.5074, 0.1278],
 	zoom = 9
 }) => {
 	return (
 		<Map
+			className={styles.map}
 			center={getDefaultCenter(sites, defaultLocation)}
 			zoom={zoom}
-			style={{ height: height }}
 		>
 			<TitleLayers />
-			<Markers markers={sites} height={height} />
+			<Markers markers={sites} />
 		</Map>
 	)
 }
 
 LeafletMap.propTypes = {
 	markers: PropTypes.array,
-	height: PropTypes.string,
 	defaultLocation: PropTypes.array,
 	zoom: PropTypes.number
 }
