c01c428450325576903f445152b60cac4fed6f98
James Singleton <jpsingleton@gmail.com>
2020-05-05 15:38:03 +0100

    make sign in a link to future login route

--- dodo-frontend/src/app/dodo/header/login/login.jsx
+++ dodo-frontend/src/app/dodo/header/login/login.jsx
@@ -1,9 +1,14 @@
 import React from "react"
 import styles from "./login.module.scss"
 import { useTranslation } from "react-i18next"
+import { Link } from "react-router-dom"
 
 export const Login = () => {
 	const { t } = useTranslation("ui")
 
-	return <div className={styles.login}>{t("header_sign_in_text")}</div>
+	return (
+		<Link to="/login" className={styles.login}>
+			{t("header_sign_in_text")}
+		</Link>
+	)
 }
