1678df9f2bdd00e77a6434c3aab602b2daf7dae6
cowtrix <seandgfinnegan@gmail.com>
2020-05-27 18:42:33 +0100

    Changed api metadata provider, will now provide resource types and other metadata at /api/

---
+++ src/DodoServer/Controllers/APIController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.Extensions;
+using Common;
+using Dodo;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DodoServer
+{
+	[Route(DodoServer.API_ROOT)]
+	[AllowAnonymous]
+	public class APIController : Controller
+	{
+		static APIController()
+		{
+			MetadataObject = new
+			{
+				ResourceTypes = ReflectionExtensions.GetConcreteClasses<IDodoResource>().
+				Select(t => new { Name = t.GetName(), Type = t.Name }).ToList(),
+			};
+		}
+
+		static dynamic MetadataObject { get; set; }
+
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Content(JsonConvert.SerializeObject(MetadataObject, JsonExtensions.NetworkSettings));
+		}
+	}
+}
