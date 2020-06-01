c1943fc4a8c9c02235d958edb64ffbf50b41f59a
cowtrix <seandgfinnegan@gmail.com>
2020-06-01 17:09:48 +0100

    Added display colours to api metadata endpoint.

--- src/DodoServer/Controllers/APIController.cs
+++ src/DodoServer/Controllers/APIController.cs
@@ -9,6 +9,11 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Dodo.Users;
+using Dodo.Rebellions;
+using Dodo.LocalGroups;
+using Dodo.WorkingGroups;
+using Dodo.Sites;
+using Dodo.Roles;
 
 namespace DodoServer
 {
@@ -16,13 +21,24 @@ namespace DodoServer
 	[AllowAnonymous]
 	public class APIController : Controller
 	{
+		static Dictionary<Type, string> m_displayColors = new Dictionary<Type, string>()
+		{
+			{ typeof(Rebellion),	"22A73D" },
+			{ typeof(LocalGroup),	"71D0F1" },
+			{ typeof(WorkingGroup), "FFC113" },
+			{ typeof(EventSite),   "ED9BC4" },
+			{ typeof(MarchSite),   "ED9BC4" },
+			{ typeof(PermanentSite),   "22A73D" },
+			{ typeof(Role),   "E53D33" },
+		};
+
 		static APIController()
 		{
 			MetadataObject = new
 			{
 				resourceTypes = ReflectionExtensions.GetConcreteClasses<IDodoResource>()
 					.Where(t => t != typeof(User))
-					.Select(t => new { label = t.GetName(), value = t.Name.ToCamelCase() }).ToList(),
+					.Select(t => new { label = t.GetName(), value = t.Name.ToCamelCase(), displayColor = m_displayColors[t] }).ToList(),
 			};
 		}
 
