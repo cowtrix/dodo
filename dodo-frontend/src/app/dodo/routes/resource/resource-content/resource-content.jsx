8c276c05b81513bbab677a63dffe72b34c802246
charlestupman <charlie@liontreestudios.co.uk>
2020-06-09 16:23:18 +0100

    fixed build errors

--- dodo-frontend/src/app/dodo/routes/resource/resource-content/resource-content.jsx
+++ dodo-frontend/src/app/dodo/routes/resource/resource-content/resource-content.jsx
@@ -1,29 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Loader, DateLayout, PageTitle } from "app/components"
-import { List, Header, Description, SignUpButton, Video } from "app/components/resources"
+import { Loader, DateLayout, PageTitle, Video, ExpandableList } from "app/components"
+import { Header, Description, SignUpButton, ParentLink } from "app/components/resources"
 
 import styles from './resource-content.module.scss'
-import { ParentLink } from '../../../../components/resources/parent-link'
+
 
 const JOIN_US_SITES = "Join us at a protest site"
 const COME_TO_EVENT = "Come to an resources"
 const VOLUNTEER_NOW = "Volunteer now with a working group"
 
-export const ResourceContent = ({ resource, setCenterMap, resourceTypes}) =>
+export const ResourceContent = ({ resource, setCenterMap, resourceTypes, resourceColor}) =>
 	<div className={styles.resource}>
-		<Header resource={resource} setCenterMap={setCenterMap} />
+		<Header resource={resource} setCenterMap={setCenterMap} resourceColor={resourceColor} />
 		<ParentLink parent={resource.parent}/>
 		<Video videoEmbedURL={resource.videoEmbedURL} />
 		<Description description={resource.publicDescription} />
-		<SignUpButton />
-		<List resources={resource.resources} title={COME_TO_EVENT} resourceTypes={resourceTypes} />
-		<List resources={resource.sites} title={JOIN_US_SITES} resourceTypes={resourceTypes} />
-		<List resources={resource.workingGroups} title={VOLUNTEER_NOW} resourceTypes={resourceTypes} />
+		<SignUpButton resourceColor={resourceColor} />
+		<ExpandableList resources={resource.resources} title={COME_TO_EVENT} resourceTypes={resourceTypes} />
+		<ExpandableList resources={resource.sites} title={JOIN_US_SITES} resourceTypes={resourceTypes} />
+		<ExpandableList resources={resource.workingGroups} title={VOLUNTEER_NOW} resourceTypes={resourceTypes} />
 	</div>
 
 ResourceContent.propTypes = {
 	resource: PropTypes.object,
+	resourceColor: PropTypes.string,
 	resourceTypes: PropTypes.array,
 	setCenterMap: PropTypes.func
 }
