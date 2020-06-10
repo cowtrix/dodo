aa5ddcdf7b771c2fd8832901f479aaa696cd1fbf
cowtrix <seandgfinnegan@gmail.com>
2020-06-10 15:22:04 +0100

    Revert "Some more AOT work on creation views."
    This reverts commit bf9cabbbe57e49bc37c7112907acfce8ad60ce4d.

--- src/DodoCMS/Edit/RebellionController.cs
+++ src/DodoCMS/Edit/RebellionController.cs
@@ -1,5 +1,11 @@
+using Dodo.LocalGroups;
+using Dodo.LocationResources;
 using Dodo.Rebellions;
+using Dodo.Roles;
+using Dodo.WorkingGroups;
 using DodoResources;
+using DodoResources.Roles;
+using DodoResources.Sites;
 using Resources;
 
 namespace Dodo.Controllers.Edit
@@ -8,5 +14,35 @@ namespace Dodo.Controllers.Edit
 	{
 		protected override AuthorizationService<Rebellion, RebellionSchema> AuthService =>
 			new GroupResourceAuthService<Rebellion, RebellionSchema>();
+	}
+
+	public class WorkingGroupController : CrudController<WorkingGroup, WorkingGroupSchema>
+	{
+		protected override AuthorizationService<WorkingGroup, WorkingGroupSchema> AuthService =>
+			new GroupResourceAuthService<WorkingGroup, WorkingGroupSchema>();
+	}
+
+	public class LocalGroupController : CrudController<LocalGroup, LocalGroupSchema>
+	{
+		protected override AuthorizationService<LocalGroup, LocalGroupSchema> AuthService =>
+			new GroupResourceAuthService<LocalGroup, LocalGroupSchema>();
+	}
+
+	public class EventController : CrudController<Event, EventSchema>
+	{
+		protected override AuthorizationService<Event, EventSchema> AuthService =>
+			new OwnedResourceAuthService<Event, EventSchema>();
+	}
+
+	public class SiteController : CrudController<Site, SiteSchema>
+	{
+		protected override AuthorizationService<Site, SiteSchema> AuthService =>
+			new OwnedResourceAuthService<Site, SiteSchema>();
+	}
+
+	public class RoleController : CrudController<Role, RoleSchema>
+	{
+		protected override AuthorizationService<Role, RoleSchema> AuthService =>
+			new RoleAuthService();
 	}
 }
