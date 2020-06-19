eb2396420e8936dd6844f0bde7f6093796d7d484
cowtrix <seandgfinnegan@gmail.com>
2020-06-19 14:30:11 +0100

    Actually added favicon. Added RememberMe field to LoginModel

--- src/Dodo/Models/LoginModel.cs
+++ src/Dodo/Models/LoginModel.cs
@@ -8,5 +8,6 @@ namespace Dodo.Models
 		public string Username { get; set; }
 		public string Password { get; set; }
 		public string Redirect { get; set; }
+		public bool RememberMe { get; set; }
 	}
 }
