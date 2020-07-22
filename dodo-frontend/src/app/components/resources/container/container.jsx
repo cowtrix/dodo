0378e9b97d2f59771de813e964738bfbbc34fe84
charlestupman <charlie@liontreestudios.co.uk>
2020-07-22 11:41:59 +0100

    fixed parent link bugs

--- dodo-frontend/src/app/components/resources/container/container.jsx
+++ dodo-frontend/src/app/components/resources/container/container.jsx
@@ -2,13 +2,12 @@ import React from "react"
 import PropTypes from "prop-types"
 import styles from "./container.module.scss"
 
-export const Container = ({ content, map = true }) => (
-    <div className={map ? styles.wrapper : styles.wrapperWithoutMap}>
-        <div className={styles.eventListContainer}>{content}</div>
-    </div>
-)
+export const Container = ({ content, hideMap }) =>
+	<div className={hideMap ? styles.wrapperWithoutMap : styles.wrapper}>
+		<div className={styles.eventListContainer}>{content}</div>
+	</div>
 
 Container.propTypes = {
-    content: PropTypes.node,
-    map: PropTypes.bool
+	content: PropTypes.node,
+	map: PropTypes.bool
 }
--- dodo-frontend/src/app/dodo/routes/resource/resource.jsx
+++ dodo-frontend/src/app/dodo/routes/resource/resource.jsx
@@ -5,84 +5,83 @@ import { SiteMap, Loader } from "app/components"
 import { ResourceContent } from './resource-content'
 
 export const Resource =
-    (
-        {
-            match,
-            getResource,
-            getNotifications,
-            resource,
-            notifications,
-            isLoading,
-            centerMap,
-            setCenterMap,
-            resourceTypes = [],
-            subscribeResource,
-            leaveResource,
-            memberOf = [],
-            isLoggedIn,
-            fetchingUser
-        }
-    ) => {
-        const { resourceId, resourceType } = match.params
+	(
+		{
+			match,
+			getResource,
+			getNotifications,
+			resource,
+			notifications,
+			isLoading,
+			centerMap,
+			setCenterMap,
+			resourceTypes = [],
+			subscribeResource,
+			leaveResource,
+			memberOf = [],
+			isLoggedIn,
+			fetchingUser
+		}
+	) => {
+		const { resourceId, resourceType } = match.params
 
-        useEffect(() => {
-            getResource(resourceType, resourceId, setCenterMap)
-            getNotifications(resourceType, resourceId);
-        }, [match])
+		useEffect(() => {
+			getResource(resourceType, resourceId, setCenterMap)
+			getNotifications(resourceType, resourceId);
+		}, [match])
 
 
-        const resourceColor =
-            resource.metadata &&
-            resourceTypes.length &&
-            '#' + resourceTypes.find(resType => resource.metadata.type === resType.value).displayColor
+		const resourceColor =
+			resource.metadata &&
+			resourceTypes.length &&
+			'#' + resourceTypes.find(resType => resource.metadata.type === resType.value).displayColor
 
-        const { location } = resource
-        const defaultLocation = resource.location
-            ? [location.latitude, location.longitude]
-            : []
+		const { location } = resource
+		const defaultLocation = resource.location
+			? [location.latitude, location.longitude]
+			: []
 
-        const currentResourceType = resource && resource.metadata ? resource.metadata.type : undefined;
+		const hideMap = resourceType === 'role' || resourceType === 'workingGroup'
 
-        return (
-            <Fragment>
-                {currentResourceType === "role" || currentResourceType === "workingGroup" ? null :
-                    <SiteMap
-                        centerMap={centerMap}
-                        setCenterMap={setCenterMap}
-                        center={defaultLocation}
-                        sites={resource.sites && [...resource.sites, ...resource.workingGroups]}
-                    />
-                }
-                <Container
-                    map={currentResourceType === "role" || currentResourceType === "workingGroup" ? false : true}
-                    content={
-                        <Fragment>
-                            <Loader display={isLoading || fetchingUser} />
-                            {resource.metadata && !isLoading && (
-                                <ResourceContent
-                                    resource={resource}
-                                    notifications={notifications}
-                                    setCenterMap={setCenterMap}
-                                    resourceTypes={resourceTypes}
-                                    resourceColor={resourceColor}
-                                    resourceType={resourceType}
-                                    subscribeResource={subscribeResource}
-                                    leaveResource={leaveResource}
-                                    memberOf={memberOf}
-                                    isLoggedIn={isLoggedIn}
-                                />
-                            )}
-                        </Fragment>
-                    }
-                />
-            </Fragment>
-        )
-    }
+		return (
+			<Fragment>
+				<SiteMap
+					display={!hideMap}
+					centerMap={centerMap}
+					setCenterMap={setCenterMap}
+					center={defaultLocation}
+					sites={resource.sites && [...resource.sites, ...resource.workingGroups]}
+				/>
+				<Container
+					hideMap={hideMap}
+					content={
+						<Fragment>
+							<Loader display={isLoading || fetchingUser}/>
+							{resource.metadata && !isLoading && (
+								<ResourceContent
+									resource={resource}
+									notifications={notifications}
+									setCenterMap={setCenterMap}
+									resourceTypes={resourceTypes}
+									resourceColor={resourceColor}
+									resourceType={resourceType}
+									subscribeResource={subscribeResource}
+									leaveResource={leaveResource}
+									memberOf={memberOf}
+									isLoggedIn={isLoggedIn}
+								/>
+							)}
+						</Fragment>
+					}
+				/>
+			</Fragment>
+		)
+	}
 
 Resource.propTypes = {
-    match: PropTypes.object.isRequired,
-    getResource: PropTypes.func,
-    event: PropTypes.object,
-    resourceTypes: PropTypes.array,
-    fetchingUser: PropTypes.bool
+	match: PropTypes.object.isRequired,
+	getResource: PropTypes.func,
+	event: PropTypes.object,
+	resourceTypes: PropTypes.array,
+	fetchingUser: PropTypes.bool
 }
