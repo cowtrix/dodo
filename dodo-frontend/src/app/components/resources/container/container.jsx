357e9a8a7535c5d2d5256f1549e6ad828f976a13
Daniel Norris <dan.norris@hotmail.com>
2020-07-22 10:47:15 +0100

    added a ternary to hide map on roles and working group pages

--- dodo-frontend/src/app/components/resources/container/container.jsx
+++ dodo-frontend/src/app/components/resources/container/container.jsx
@@ -2,12 +2,13 @@ import React from "react"
 import PropTypes from "prop-types"
 import styles from "./container.module.scss"
 
-export const Container = ({ content }) => (
-	<div className={styles.wrapper}>
-		<div className={styles.eventListContainer}>{content}</div>
-	</div>
+export const Container = ({ content, map = true }) => (
+    <div className={map ? styles.wrapper : styles.wrapperWithoutMap}>
+        <div className={styles.eventListContainer}>{content}</div>
+    </div>
 )
 
 Container.propTypes = {
-	content: PropTypes.node
+    content: PropTypes.node,
+    map: PropTypes.bool
 }
