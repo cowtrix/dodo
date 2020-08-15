2891a3476e168a4ca482e65f767e2859bbddb242
Alex Davies <alexdavieswebdev@gmail.com>
2020-08-15 12:43:54 +0100

    [Resource] - Updated code that hides the map for certain resource types (which comes from URL) to be case insensitive

--- dodo-frontend/src/app/dodo/routes/resource/services.js
+++ dodo-frontend/src/app/dodo/routes/resource/services.js
@@ -4,4 +4,6 @@ export const getResourceTypeData = (resourceTypes, resourceTypeId) => {
     return resourceTypes.find(resourceType => resourceTypeId.toLowerCase() === resourceType.value.toLowerCase()) || {};
 }
 
-export const shouldHideMap = (resourceType) => resourcesWithoutMaps.find(resource => resource === resourceType)
+export const shouldHideMap = (resourceType) => {
+    return resourcesWithoutMaps.find(resource => resource.toLowerCase() === resourceType.toLowerCase())
+}
