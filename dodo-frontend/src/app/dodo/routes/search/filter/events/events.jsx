78250522acd4b1ee812fcf6be44b5b2fd855236e
Finn Merlett <finn.merlett@gmail.com>
2020-09-30 19:46:21 +0100

    stopped input expanding, capped input chars, debounced search

--- dodo-frontend/src/app/dodo/routes/search/filter/events/events.jsx
+++ dodo-frontend/src/app/dodo/routes/search/filter/events/events.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Select from "react-select"
 import styles from "./events.module.scss"
+import { RSMaxInput } from "app/components/forms"
 
 const placeholder = "Event types..."
 
@@ -13,8 +14,12 @@ export const Events = ({ resourceTypes, searchParams, search }) => {
 			value={searchParams.types}
 			options={resourceTypes}
 			className={styles.selector}
-			onChange={value => search({ ...searchParams, types: value && value.length ? value : [], search: "" })
-			}
+			onChange={value => search({
+				...searchParams,
+				types: value && value.length ? value : [],
+				search: "" 
+			})}
+			components={{Input: RSMaxInput}}
 		/>
 	)
 }
