dc622bca8964b7d516b585fca4994ea0a44d91d6
Alex Davies <alexdavieswebdev@gmail.com>
2020-10-20 22:31:02 +0100

    [Input] - Added the ability to show messages underneath the input field

--- dodo-frontend/src/app/components/forms/input/input.jsx
+++ dodo-frontend/src/app/components/forms/input/input.jsx
@@ -2,21 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './input.module.scss'
 
-export const Input = ({ name, id, type, value, setValue, maxLength, error }) =>
+export const Input = ({ name, id, type, value, setValue, maxLength, error, message }) =>
 	<div className={styles.inputWrapper}>
 		<label htmlFor={id} className={error ? styles.error : ''}>
 			<h3>{name}</h3>
 		</label>
-		<input
-			type={type}
-			id={id}
-			name={name}
-			value={value}
-			onChange={e => setValue(e.target.value)}
-			className={error ? styles.error : ''}
-			maxLength={maxLength}
-		/>
+		<div className={styles.fieldWrapper}>
+			<input
+				type={type}
+				id={id}
+				name={name}
+				value={value}
+				onChange={e => setValue(e.target.value)}
+				className={error ? styles.error : ''}
+				maxLength={maxLength}
+			/>
+			{message && <div className={`${styles.message} ${error ? styles.error : ''}`}>{message}</div>}
+		</div>
 	</div>
+
 
 Input.propTypes = {
 	type: PropTypes.string,
@@ -25,5 +29,6 @@ Input.propTypes = {
 	value: PropTypes.string,
 	error: PropTypes.bool,
 	setValue: PropTypes.func,
-	maxLength: PropTypes.number
+	maxLength: PropTypes.number,
+	message: PropTypes.string
 }
