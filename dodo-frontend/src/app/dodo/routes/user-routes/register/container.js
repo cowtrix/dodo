8300ba7a17c8b25afca17985e455634cdc7fc987
Alex Davies <alexdavieswebdev@gmail.com>
2020-11-10 10:30:43 +0000

    [Register] - Added tickbox for terms and conditions agreement that must be ticked to proceed with registration

--- dodo-frontend/src/app/dodo/routes/user-routes/register/container.js
+++ dodo-frontend/src/app/dodo/routes/user-routes/register/container.js
@@ -1,14 +1,17 @@
 import { connect } from 'react-redux'
 import { Register } from './register'
+import { selectors as resourceSelectors } from "app/domain/resources"
 import { actions, selectors } from 'app/domain/user/index'
 
 const { username, registeringUser, registerError } = selectors
 const { registerUser } = actions
+const { privacyPolicy } = resourceSelectors
 
 const mapStateToProps = state => ({
 	isLoggedIn: username(state),
 	registeringUser: registeringUser(state),
-	error: registerError(state)
+	error: registerError(state),
+	privacyPolicy: privacyPolicy(state)
 })
 
 const mapDispatchToProps = dispatch => ({
