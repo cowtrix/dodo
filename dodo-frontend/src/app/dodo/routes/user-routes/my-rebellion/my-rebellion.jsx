adb99aafa72559d3242203d0f8721b6f089c6f31
Alex Davies <alexdavieswebdev@gmail.com>
2020-12-19 23:29:02 +0000

    [MyRebellion] - Added collapsible tree that displays the users subscriptions

--- dodo-frontend/src/app/dodo/routes/user-routes/my-rebellion/my-rebellion.jsx
+++ dodo-frontend/src/app/dodo/routes/user-routes/my-rebellion/my-rebellion.jsx
@@ -1,30 +1,73 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { Container } from 'app/components/forms/index'
 import { Resource } from './resource'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
+
+import styles from './my-rebellion.module.scss';
 
 const MY_REBELLION = "My rebellion"
 
-export const MyRebellion = ({ error, getMyRebellion, isFetching, myRebellion, resourceTypes }) => {
+function getSubscriptionTree(subscriptions, resourceTypes, level = 'root', closed, onToggleList) {
+	return (
+		<ul className={`${styles.tree} ${styles[level]}`}>
+			{subscriptions.map(subscription => {
+				const isClosed = closed.includes(subscription.reference.guid);
+
+				return (
+					<>
+						<li>
+							<button
+								className={styles.button}
+								onClick={() => onToggleList(subscription.reference.guid, !isClosed)}
+								disabled={subscription.children.length === 0}
+								aria-hidden={true}>
+								<FontAwesomeIcon icon={isClosed ? faPlus : faMinus} />
+							</button>
+							<Resource {...subscription.reference} resourceTypes={resourceTypes}/>
+						</li>
+						{subscription.children.length > 0 && (
+							<li className={isClosed ? styles.hidden : ''}>
+								{getSubscriptionTree(subscription.children, resourceTypes, 'child', closed, onToggleList)}
+							</li>
+						)}
+					</>
+				);
+			})}
+		</ul>
+	);
+}
+
+export const MyRebellion = ({ error, getMyRebellion, isFetching, myRebellion = [], resourceTypes }) => {
 	useEffect(() => { getMyRebellion() }, []);
+
+	const [closed, setClosed] = useState([]);
+
+	const onToggleList = (guid, closing) => {
+		if(closing) {
+			setClosed([...closed, guid]);
+		} else {
+			setClosed(closed.filter(listGuid => listGuid !== guid));
+		}
+	}
 
 	let content;
 	if(isFetching) {
-		content = <>Loading...</>;
+		content = <p>Loading, please wait...</p>;
 	}
 	else if(error) {
 		content = <>There was an error loading the data.</>;
 	}
 	else {
-		content = (
-			<>
-				{/* {memberOf.map(resource =>
-					<Resource {...resource} resourceTypes={resourceTypes}/>
-				)} */}
-				{'data = ' + JSON.stringify(myRebellion)}
-			</>
-		)
+		content = <>
+			{myRebellion.length ? (
+				<>{getSubscriptionTree(myRebellion, resourceTypes, 'root', closed, onToggleList)}</>
+			) : (
+				<p>You do not currently have any subscriptions.</p>
+			)}
+		</>;
 	}
 
 	return (
@@ -32,7 +75,8 @@ export const MyRebellion = ({ error, getMyRebellion, isFetching, myRebellion, re
 	)
 }
 
-
 MyRebellion.propTypes = {
 	user: PropTypes.object
 }
+
+export default MyRebellion;
