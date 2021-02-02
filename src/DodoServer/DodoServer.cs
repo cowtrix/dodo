0412dbcedea154ceaf9985bccd7d7cc4f629fb2a
cowtrix <seandgfinnegan@gmail.com>
2021-02-02 16:14:12 +0000

    Added step for registering sysadmin as first user.

--- src/DodoServer/DodoServer.cs
+++ src/DodoServer/DodoServer.cs
@@ -1,11 +1,15 @@
 using Common;
 using Common.Config;
 using Common.Extensions;
+using Dodo;
 using Dodo.Expiry;
 using Dodo.Security;
+using Dodo.Users;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json;
+using Resources;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -23,7 +27,30 @@ namespace DodoServer
 #endif
 			SessionTokenStore.Initialise();
 			ExpiryWorker.Initialise();
+
+			var usrManager = ResourceUtility.GetManager<User>();
+			if(usrManager.Count == 0)
+			{
+				GenerateSysadmin();
+			}
+
 			CreateHostBuilder(args).Build().Run();
+		}
+
+		private static void GenerateSysadmin()
+		{
+			// Generate sysadmin account if no account is registered and print
+			var schema = new UserSchema($"admin_{DodoApp.ServerSalt.Substring(0, 6)}", ValidationExtensions.GenerateStrongPassword(), DodoApp.DevEmail);
+			ResourceUtility.GetFactory<User>()
+				.CreateTypedObject(new ResourceCreationRequest(default, schema));
+			Console.BackgroundColor = ConsoleColor.Red;
+			Console.ForegroundColor = ConsoleColor.Black;
+			Console.WriteLine($"THIS IS IMPORTANT. A system admin account has been generated.");
+			Console.WriteLine($"Username: {schema.Username}");
+			Console.WriteLine($"Email: {schema.Email}");
+			Console.WriteLine($"Password: {schema.Password}");
+			Console.WriteLine($"THE FIRST THING YOU SHOULD DO IS CHANGE THIS PASSWORD.");
+			Console.ResetColor();
 		}
 
 		private static IEnumerable<string> GetUrls()
