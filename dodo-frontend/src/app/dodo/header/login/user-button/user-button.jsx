0c05bb355d2d6fe1e0bc59a667da2709398b834d
robp <rprnc082@gmail.com>
2021-03-30 18:24:19 +0000

    Update user-button.jsx

--- dodo-frontend/src/app/dodo/header/login/user-button/user-button.jsx
+++ dodo-frontend/src/app/dodo/header/login/user-button/user-button.jsx
@@ -7,12 +7,15 @@ import styles from './user-button.module.scss'
 const USER_BUTTON_ALT = "Button for user menu"
 
 export const UserButton = ({ notifications, setMenuOpen, menuOpen }) =>
-	<img
-		src={userButton}
-		alt={USER_BUTTON_ALT}
-		className={styles.userButton}
-		onClick={() => setMenuOpen(!menuOpen)}
-	/>
+	<>
+		<h4>PROFILE</h4>
+		<img
+			src={userButton}
+			alt={USER_BUTTON_ALT}
+			className={styles.userButton}
+			onClick={() => setMenuOpen(!menuOpen)}
+		/>
+	</>
 
 UserButton.propTypes = {
 	notifications: PropTypes.array,
