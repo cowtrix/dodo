52380891c6e46fcac44ca3d4d83456d1f572af70
Alex Davies <alexdavieswebdev@gmail.com>
2021-04-19 21:08:48 +0100

    [UserMenu] - Menu will now close after you press 'logout'

--- dodo-frontend/src/app/dodo/user-menu/menu/options/options.jsx
+++ dodo-frontend/src/app/dodo/user-menu/menu/options/options.jsx
@@ -6,18 +6,26 @@ import { Link } from 'react-router-dom'
 import { MY_REBELLION_ROUTE } from '../../../routes/user-routes/my-rebellion'
 import { SETTINGS_ROUTE } from '../../../routes/user-routes'
 
-export const Options = ({ logout, closeMenu }) =>
-	<ul className={styles.options}>
-		<li>
-			<Link onClick={() => closeMenu()} to={MY_REBELLION_ROUTE}>My Subscriptions</Link>
-		</li>
-		<li>
-			<Link onClick={() => closeMenu()} to={SETTINGS_ROUTE}>Profile Settings</Link>
-		</li>
-		<li>
-			<Link onClick={() => logout()} to="/">Logout</Link>
-		</li>
-	</ul>
+export const Options = ({ logout, closeMenu }) => {
+	const handleLogout = () => {
+		logout();
+		closeMenu();
+	}
+
+	return (
+		<ul className={styles.options}>
+			<li>
+				<Link onClick={() => closeMenu()} to={MY_REBELLION_ROUTE}>My Subscriptions</Link>
+			</li>
+			<li>
+				<Link onClick={() => closeMenu()} to={SETTINGS_ROUTE}>Profile Settings</Link>
+			</li>
+			<li>
+				<Link onClick={() => handleLogout()} to="/">Logout</Link>
+			</li>
+		</ul>
+	);
+}
 
 Options.propTypes = {
 	logout: PropTypes.func,
